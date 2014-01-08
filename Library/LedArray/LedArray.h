5ea4fbc6f4a6ed6a7de8d29e5d5a60ec9b508729
Jérôme GROSSÉ <jerome.l.grosse@gmail.com>
2014-01-08 01:27:55 +0100

    Added the 'LedArray' Library

---
+++ Library/LedArray/LedArray.h
@@ -0,0 +1,30 @@
+/*
+    LedArray.h - Library for some basic function with a led array.
+    Created by Jérôme Grossé, January 2014.
+*/
+
+#ifndef LedArray_h
+#define LedArray_h
+
+
+#include "Arduino.h"
+
+class LedArray
+{
+    public:
+        LedArray(int row[], int col[], int width, int height);
+        void clear_display();
+        void default_display();
+        void display_screen(byte* data);
+        byte reverse_byte(byte x);
+    private:
+        unsigned int _timestamp;
+        int* _row;
+        int* _col;
+        int  _width;
+        int  _height;
+        void display_line(byte data, int row_number);
+};
+
+
+#endif
---
+++ Library/LedArray/README.md
@@ -0,0 +1,14 @@
+LedArray Library
+================
+
+Library tested on a 8x8 array.
+
+The constructor needs the following:
+    - row:    An array of integer giving the pin number for each row number.
+    - col:    An array of integer giving the pin number for each row number.
+    - width:  The width of the array of LED.
+    - height: The hight of the array of LED.
+
+
+
+An exemple of this library in use can be found in the project: 8x8 LED Array.
