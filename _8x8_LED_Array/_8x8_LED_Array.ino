2b3e45d688b3868c10be67b26543e37be59063d6
Jérôme GROSSÉ <jerome.l.grosse@gmail.com>
2014-01-08 14:18:01 +0100

    Updated 8x8 Led Array Project according to the recent modifications of the library it uses

--- _8x8_LED_Array/_8x8_LED_Array.ino
+++ _8x8_LED_Array/_8x8_LED_Array.ino
@@ -22,80 +22,9 @@ int col[] = {col1, col2, col3, col4, col5, col6, col7, col8};
 
 LedArray ledArray(row, col, 8, 8);
 
-//Ascii char to be displayed
-byte ascii_data_H[] = {
-    0x11,
-    0x11,
-    0x11,
-    0x1F,
-    0x11,
-    0x11,
-    0x11,
-    0x00
-};
-
-byte ascii_data_i[] = {
-    0x04,
-    0x00,
-    0x0C,
-    0x04,
-    0x04,
-    0x04,
-    0x0E,
-    0x00
-};
-
-byte ascii_data_t[] = {
-    0x08,
-    0x08,
-    0x1C,
-    0x08,
-    0x08,
-    0x09,
-    0x06,
-    0x00
-};
-
-byte ascii_data_h[] = {
-    0x10,
-    0x10,
-    0x16,
-    0x19,
-    0x11,
-    0x11,
-    0x11,
-    0x00
-};
-
-byte ascii_data_e[] = {
-    0x00,
-    0x00,
-    0x0E,
-    0x11,
-    0x1F,
-    0x10,
-    0x0E,
-    0x00
-};
-
-byte ascii_data_r[] = {
-    0x00,
-    0x00,
-    0x16,
-    0x19,
-    0x10,
-    0x10,
-    0x10,
-    0x00
-};
-
-byte* ascii_data[] = {ascii_data_H, ascii_data_i, ascii_data_t, ascii_data_h, ascii_data_e, ascii_data_r, ascii_data_e};
-
-
 //Timing variable
 unsigned int timestamp_1 = millis();
 unsigned int timestamp_2;
-
 
 
 void setup(){
@@ -103,8 +32,6 @@ void setup(){
 }
 
 void loop(){
-
     timestamp_2 = millis();
-    //ledArray.display_screen(ascii_data[ (timestamp_2 - timestamp_1)/1000 % 6 ], true);
-    ledArray.default_display();
+    ledArray.print_string("TestDisplay", 11, 750);
 }
