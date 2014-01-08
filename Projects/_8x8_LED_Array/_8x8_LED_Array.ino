92c3f988356b137d992de6cf08735623ce17fe4b
Jérôme GROSSÉ <jerome.l.grosse@gmail.com>
2014-01-08 15:33:34 +0100

    Project 8x8 LED Display: Clarification in the display configuration

--- Projects/_8x8_LED_Array/_8x8_LED_Array.ino
+++ Projects/_8x8_LED_Array/_8x8_LED_Array.ino
@@ -1,22 +1,42 @@
 #include <LedArray.h>
 
-//Led settings
-int col1 = 2;
-int col2 = 11;
-int col3 = 10;
-int col4 = A2;
-int col5 = 8;
-int col6 = A1;
-int col7 = 4;
-int col8 = 5;
-int row1 = A3;
-int row2 = 3;
-int row3 = 6;
-int row4 = A0;
-int row5 = 13;
-int row6 = 7;
-int row7 = 12;
-int row8 = 9;
+//Arduino - Display pin mapping.
+int pin_1  = 13;
+int pin_2  = 12;
+int pin_3  = 11;
+int pin_4  = 10;
+int pin_5  = 9;
+int pin_6  = 8;
+int pin_7  = 7;
+int pin_8  = 6;
+int pin_9  = A3;
+int pin_10 = A2;
+int pin_11 = A1;
+int pin_12 = A0;
+int pin_13 = 2;
+int pin_14 = 3;
+int pin_15 = 4;
+int pin_16 = 5;
+
+//Device-related configuration:
+int col1 = pin_13;
+int col2 = pin_3;
+int col3 = pin_4;
+int col4 = pin_10;
+int col5 = pin_6;
+int col6 = pin_11;
+int col7 = pin_15;
+int col8 = pin_16;
+int row1 = pin_9;
+int row2 = pin_14;
+int row3 = pin_8;
+int row4 = pin_12;
+int row5 = pin_1;
+int row6 = pin_7;
+int row7 = pin_2;
+int row8 = pin_5;
+
+
 int row[] = {row1, row2, row3, row4, row5, row6, row7, row8};
 int col[] = {col1, col2, col3, col4, col5, col6, col7, col8};
 
