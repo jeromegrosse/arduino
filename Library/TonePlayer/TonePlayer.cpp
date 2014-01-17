70c315a6e0e4a4f32a8593bd3c740fa7cb99b8dc
Jérôme GROSSÉ <jerome.l.grosse@gmail.com>
2014-01-17 17:46:53 +0100

    TonePlayer: Possibility to have 2 channels playing at the same time.

--- Library/TonePlayer/TonePlayer.cpp
+++ Library/TonePlayer/TonePlayer.cpp
@@ -8,16 +8,29 @@
  * int* durations: Array of duration related to the notes defined above.
  * int duration  : Duration of the whole track (i.e. The length of the notes/durations array).
  */
-TonePlayer::TonePlayer(int pin, int notes[], int durations[], int duration){
-    _pin       = pin;
-    _notes      = notes;
-    _durations = durations;
-    _duration  = duration;
+TonePlayer::TonePlayer(int pin, prog_uint16_t notes[], prog_uint16_t durations[], int duration, int timer, bool timestamp){
+    _pin                = pin;
+    _notes              = notes;
+    _durations          = durations;
+    _duration           = duration;
+    _timer              = timer;
+    _timestamp_duration = timestamp;
+    _start_timestamp   = 0;
 
-    _pointer        = 0;
-    _tempoCorrector = 1;
+    _pointer           = 0;
+    _tempoCorrector    = 1;
+    _time_left_to_play = 0;
 
-    pinMode(pin, OUTPUT);
+    pinMode(_pin, OUTPUT);
+
+    if(_timer == 1){
+        TCCR1A = _BV(COM1A0) | _BV(WGM13) | _BV(WGM10);
+        OCR1B  = 100;
+    } else if(_timer == 2){
+        TCCR2A = _BV(COM2A0) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+        OCR2B  = 100;
+    }
+
 }
 
 
@@ -25,7 +38,13 @@ TonePlayer::TonePlayer(int pin, int notes[], int durations[], int duration){
  * Returns a boolean telling whether the track is finished
  */
 bool TonePlayer::finished(){
-    return _pointer == _duration;
+    int finished = _pointer >= _duration;
+
+    if(finished){
+        if(_timer == 1) OCR1A = 0;
+        if(_timer == 2) OCR2A = 0;
+    }
+    return finished;
 }
 
 
@@ -33,16 +52,114 @@ bool TonePlayer::finished(){
  * Plays the next note
  */
 void TonePlayer::playNote(){
-    if(_pointer == _duration) return;
-    int noteDuration = _durations[_pointer] * _tempoCorrector;
-    tone(_pin, _notes[_pointer], noteDuration);
+    if(finished()) return;
+    int timestamp = millis();
 
-    delay(noteDuration * 1.30);
-    noTone(_pin);
+    if(_timestamp_duration){
+        if(_start_timestamp == 0) _start_timestamp = timestamp;
+        if(pgm_read_word_near(_durations + _pointer + 1) < timestamp - _start_timestamp){
+            _pointer++;
 
-    _pointer++;
+            if(_timer == 1)      _playNoteTimer1();
+            else if(_timer == 2) _playNoteTimer2();
+        }
+
+    }else {
+        if(_timestamp +  pgm_read_word_near(_durations + _pointer) <= timestamp){
+            _pointer++;
+            _timestamp = timestamp;
+
+            if(_timer == 1)      _playNoteTimer1();
+            else if(_timer == 2) _playNoteTimer2();
+        }
+    }
 }
 
+
+void TonePlayer::_playNoteTimer1(){
+    _frequency      = pgm_read_word_near(_notes + _pointer);
+    uint32_t ocr            = F_CPU / _frequency / 2 / 1 - 1;
+    byte prescalarbits = 0b001;
+
+    if (ocr > 0xffff)
+    {
+        ocr = F_CPU / _frequency / 2 / 8 - 1;
+        prescalarbits = 0b010;
+    }
+
+    if (ocr > 0xffff)
+    {
+        ocr = F_CPU / _frequency / 2 / 64 - 1;
+        prescalarbits = 0b011;
+    }
+
+    if (ocr > 0xffff)
+    {
+        ocr = F_CPU / _frequency / 2 / 256 - 1;
+        prescalarbits = 0b100;
+    }
+
+    if (ocr > 0xffff)
+    {
+        ocr = F_CPU / _frequency / 2 / 1024 - 1;
+        prescalarbits = 0b101;
+    }
+
+    TCCR1B = _BV(WGM13) | prescalarbits;
+    OCR1A  = ocr;
+}
+
+void TonePlayer::_playNoteTimer2(){
+    _frequency      = pgm_read_word_near(_notes + _pointer);
+    uint32_t ocr      = (F_CPU / _frequency / 2 / 1) - 1;
+    byte prescalarbits = 0b001;
+
+    if (ocr > 255)
+    {
+        ocr = (F_CPU / _frequency / 2 / 8) - 1;
+        prescalarbits = 0b010;
+    }
+
+    if (ocr > 255)
+    {
+        ocr = (F_CPU / _frequency / 2 / 32) - 1;
+        prescalarbits = 0b011;
+    }
+
+    if (ocr > 255)
+    {
+        ocr = (F_CPU / _frequency / 2 / 64) - 1;
+        prescalarbits = 0b100;
+    }
+
+    if (ocr > 255)
+    {
+        ocr = (F_CPU / _frequency / 2 / 128) - 1;
+        prescalarbits = 0b101;
+    }
+
+    if (ocr > 255)
+    {
+        ocr = (F_CPU / _frequency / 2 / 256) - 1;
+        prescalarbits = 0b110;
+    }
+
+    if (ocr > 255)
+    {
+        ocr = (F_CPU / _frequency / 2 / 1024) - 1;
+        prescalarbits = 0b111;
+    }
+
+    if (ocr > 255)
+    {
+        //The note can't be played
+        ocr = 0;
+        prescalarbits = 0b000;
+    }
+
+    TCCR2B = _BV(WGM22) | prescalarbits;
+    OCR2A  = ocr;
+}
 
 /*
  * Resets the pointer to the beginning of the track.
@@ -64,7 +181,7 @@ int TonePlayer::getPointer(){
  * Returns the current note.
  */
 int TonePlayer::getNote(){
-    return _notes[_pointer];
+    return _frequency;
 }
 
 
@@ -72,7 +189,7 @@ int TonePlayer::getNote(){
  * Returns the duration of the current note.
  */
 int TonePlayer::getDuration(){
-    return _durations[_pointer];
+    return pgm_read_word_near(_durations + _pointer);
 }
 
 
@@ -83,4 +200,18 @@ int TonePlayer::correctTempo(float correction){
     if(correction > 0){
         _tempoCorrector = correction;
     }
+}
+
+/*
+ *
+ */
+void TonePlayer::nextNote(){
+    if(finished()) return;
+    _pointer++;
+    if(_timer == 1)      _playNoteTimer1();
+    else if(_timer == 2) _playNoteTimer2();
+}
+
+int TonePlayer::getFrequency(){
+    return _frequency;
 }
--- Library/TonePlayer/TonePlayer.h
+++ Library/TonePlayer/TonePlayer.h
@@ -8,25 +8,37 @@
 
 
 #include "Arduino.h"
+#include <avr/pgmspace.h>
+
 
 class TonePlayer
 {
     public:
-        TonePlayer(int pin, int notes[], int durations[], int duration);
-        bool finished();
-        void playNote();
-        void resetPointer();
-        int  getPointer();
-        int  getNote();
-        int  getDuration();
-        int  correctTempo(float correction);
+        TonePlayer(int pin, prog_uint16_t* notes, prog_uint16_t* durations, int duration, int timer, bool timestamp);
+        bool        finished();
+        void        playNote();
+        void        resetPointer();
+        int         getPointer();
+        int         getNote();
+        int         getDuration();
+        int         correctTempo(float correction);
+        void        nextNote();
+        int         getFrequency();
     private:
         int         _pin;
-        int*        _notes;
-        int*        _durations;
+        prog_uint16_t*        _notes;
+        prog_uint16_t*        _durations;
+        int         _frequency;
         int         _duration;
         int         _pointer;
         float       _tempoCorrector;
+        int         _time_left_to_play;
+        int         _timestamp;
+        int         _timer;
+        int         _start_timestamp;
+        int         _timestamp_duration;
+        void        _playNoteTimer1();
+        void        _playNoteTimer2();
 };
 
 
